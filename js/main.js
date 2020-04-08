86f542ca9977cd1be14dddb23ed6e4bf41b5d28f
Roman Osadsky <rosadsky@hotmail.com>
2020-04-08 21:13:33 +0200

    Update main.js
    Funkčné generovanie budov

--- js/main.js
+++ js/main.js
@@ -7,15 +7,25 @@ window.onload = function () {
 
     c.width = 485; // Šírka canvasu
     c.height = 600; // Výška canvasu
-    let vyskaPrechodu = 180;
+    let vyskaPrechodu = 180; //Priestor pre vtáka v px
 
     const ctx = c.getContext('2d');
 
     const prostredie = new Prostredie(c,ctx); //c, ctx
     const vtak = new Vtak(50,250,ctx); //x,y,ctx
-    const budova = new Budova(200 ,  0,  200,    3  ,ctx);
+
+    const budovy = [];
+
+    //const budova = new Budova(200 ,  0,  200,    3  ,ctx);
                             //xpos,ypos,length,speed,ctx
                             //dialka, vyska
+
+    setInterval(function () { //Interval na každých 3000 = 3 sekundy.
+
+        let budovaSet = generovanieBudov(ctx,c.width,c.height);
+        budovy.push(budovaSet.top,budovaSet.bottom);
+    },1500) //sekundy
+
     loopHry();
 
 
@@ -24,8 +34,10 @@ window.onload = function () {
         ctx.fillRect(0,0,c.width,c.height);
         prostredie.update();
         prostredie.render();
-        budova.update();
-        budova.render();
+        budovy.forEach(function (budova) {
+            budova.update();
+            budova.render();
+        });
         vtak.update();
         vtak.render();
         window.requestAnimationFrame(loopHry);
@@ -35,9 +47,10 @@ window.onload = function () {
         let dlzkaTop = Math.round(Math.random()*200+100); //náhodné číslo okolo 300
         let dlzaBottom = 600 - vyskaPrechodu - dlzkaTop;
         let rBudova = { };
-        rBudova.top = new Budova(600, 0 , dlzkaTop, 20, ctx); // Generovanie vrchnej budovy
-        rBudova.bottom = new Budova(600, 600 - dlzaBottom, dlzaBottom, 20, ctx); // Generovanie spodnej budovy
-        
+        rBudova.top = new Budova(600, 0 , dlzkaTop, 3, ctx); // Generovanie vrchnej budovy
+        rBudova.bottom = new Budova(600, 600 - dlzaBottom, dlzaBottom, 3, ctx); // Generovanie spodnej budovy
+        return rBudova;
+
     }
 
 
