5d7060bca1da757bdf5a6329ad29df33df2bf33f
Roman Osadsky <rosadsky@hotmail.com>
2020-04-08 17:56:10 +0200

    Update Budova.js

---
+++ js/Budova.js
@@ -0,0 +1,28 @@
+
+
+
+const Budova = function (pos,length,speed,speed) {
+
+    this.pos = pos;
+    this.length = lenght;
+    this.ctx = ctx;
+    this.speed = speed;
+};
+
+Budova.prototype.update(){
+
+
+};
+
+Budova.prototype.render(){
+
+    this.ctx.save(); //uložíme hodnoty
+    this.ctx.fillStyle = "green";
+
+
+
+    this.ctx.restore();
+
+
+
+};
